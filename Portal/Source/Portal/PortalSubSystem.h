2598364755bbc474825afba9b5a06f2956811cb9
lucasdprx <150352145+lucasdprx@users.noreply.github.com>
2025-03-13 10:13:14 +0100

    Git ignore and move project

---
+++ Portal/Source/Portal/PortalSubSystem.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "PortalSubSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTAL_API UPortalSubSystem : public ULocalPlayerSubsystem
+{
+	GENERATED_BODY()
+};
